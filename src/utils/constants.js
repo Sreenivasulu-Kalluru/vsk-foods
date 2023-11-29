520dd1380a898a5ff65331e4b3f4086597121117
Vasu Kalluru <84690382+KALLURUSREENIVASULU@users.noreply.github.com>
2023-11-29 08:40:33 +0530

    cors issue resolved - bypassed cors using corsproxy.io

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,4 +5,4 @@ export const LOGO_URL =
   'https://png.pngtree.com/png-vector/20230217/ourmid/pngtree-food-logo-design-for-restaurant-and-business-png-image_6604922.png';
 
 export const MENU_API =
-  'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=';
+  'https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=';
